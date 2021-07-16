f4615751d36bdeab3eab546e10708e815bdd1cc3
Sadab <sadabmm@gmail.com>
2021-07-16 13:05:42 -0400

    Added URL and Datasource insert fields

--- src/SimplePanel.tsx
+++ src/SimplePanel.tsx
@@ -1,92 +1,87 @@
-import React, { useState } from 'react';
-import { PanelProps, DataSourceInstanceSettings, DataSourceJsonData, DataQueryRequest} from '@grafana/data';
-import { SimpleOptions, MyQuery } from 'types';
-import { getDataSourceSrv } from '@grafana/runtime';
+import React, {useState} from 'react';
+import {
+    PanelProps,
+    DataSourceInstanceSettings,
+    DataSourceJsonData,
+    DataQueryRequest
+} from '@grafana/data';
+import {SimpleOptions, MyQuery} from 'types';
+import {getDataSourceSrv} from '@grafana/runtime';
 // import { css, cx } from 'emotion';
 // import { stylesFactory, useTheme } from '@grafana/ui';
 
-interface Props extends PanelProps<SimpleOptions> {}
+interface Props extends PanelProps<SimpleOptions> {
+}
 
-export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
-  const [value, setValue] = useState('');
-  const [fetchedData/*, setFetchedData*/] = useState('');
+export const SimplePanel: React.FC<Props> = ({options, data, width, height}) => {
+    const [value, setValue] = useState('');
+    const [fetchedData , setFetchedData] = useState('');
 
-  //let URL = 'https://jsonplaceholder.typicode.com/posts/1';
+    // console.log('Data from Panel Props:')
+    // console.log(data)
 
-  // const fetchAPI = async (url: string) => {
-  //   //Request Payload for the PUT/POST req.
-  //   const sendData = {
-  //     id: 1,
-  //     title: value,
-  //     body: 'bar',
-  //     userId: 1,
-  //   };
-  //   const requestOptions = {
-  //     method: 'PUT',
-  //     headers: { 'Content-Type': 'application/json' },
-  //     body: JSON.stringify(sendData),
-  //   };
-  //   try {
-  //     const response = await fetch(url, requestOptions);
-  //     const json = await response.json();
-  //     setFetchedData(json.title); //Set the required API response data to state variable
-  //   } catch (error) {
-  //     console.log('error', error);
-  //   }
-  // };
-  const handleSubmit = (evt: { preventDefault: () => void }) => {
-    evt.preventDefault();
-    alert(`Submitting Value: ${value}`);
-    let dssrv = getDataSourceSrv();
-    console.log('dssrv is: ');
-    console.log(dssrv);
-    console.log('getList returns: ');
-    console.log(dssrv.getList());
-    console.log('get("VOLTTRON") returns:');
-    console.log(dssrv.get('VOLTTRON'))
-    console.log('getInstanceSettings returns:');
-    const instance_settings: DataSourceInstanceSettings<DataSourceJsonData> | undefined = dssrv.getInstanceSettings('VOLTTRON');
-    console.log(instance_settings)
-    //const  url = '' ? instance_settings === undefined : instance_settings.url
-    let dsprms = dssrv.get('VOLTTRON').then(
-        (DataSource)  => {
+    const handleSubmit = (evt: { preventDefault: () => void }) => {
+        evt.preventDefault();
+        alert(`Submitting Value: ${value}`);
+        let dssrv = getDataSourceSrv();
+        console.log('dssrv is: ');
+        console.log(dssrv);
+        console.log('getList returns: ');
+        console.log(dssrv.getList());
+        console.log('get("VOLTTRON") returns:');
+        console.log(dssrv.get('VOLTTRON'));
+        console.log('getInstanceSettings returns:');
+        const instance_settings: DataSourceInstanceSettings<DataSourceJsonData> | undefined =
+            dssrv.getInstanceSettings(options.DataSourceName);
+        console.log(instance_settings);
+        //const  url = '' ? instance_settings === undefined : instance_settings.url
+        let dsprms = dssrv.get(options.DataSourceName).then(async (DataSource) => {
             let q = {
                 refId: DataSource.id.toString(),
                 http_method: 'PUT',
-                route: '/platforms/sekhmet/devices/Campus/Building1/Fake2/SampleWritableFloat1',
-                data: {'value': value}
+                route: '/platforms/' + options.VolttronNode + '/devices/' + options.URL,
+                data: {value: value},
             } as MyQuery;
-          let request = {} as DataQueryRequest;
-          request.targets = [q];
-          let resp = DataSource.query(request)
-          console.log('resp from query is:')
-          console.log(resp)
-          //onRunQuery()
-          return request
-        }
-    );
-    console.log(dsprms);
-  };
+            let request = {} as DataQueryRequest;
+            request.targets = [q];
+            let resp = await DataSource.query(request);
+            console.log('resp from query is:');
+            console.log(resp);
 
-  return (
-    <div
-      style={{
-        width,
-        height,
-      }}
-    >
-      <div>
-        <h1>Set Point Value: {fetchedData}</h1>
-      </div>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <label>
-            Enter Set Point Value:
-            <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
-          </label>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-    </div>
-  );
+            let setError = JSON.parse(resp['data']['0'].first.buffer['0'])
+            console.log(setError['Campus/Building1/Fake2/SampleWritableFloat1'].set_error)
+            if (setError[options.URL].set_error == null){
+                setFetchedData(value);
+            } else {
+                setFetchedData("Error Setting Value")
+            }
+            //onRunQuery()
+            //return request;
+        })
+
+        console.log("Datasource param response")
+        console.log(dsprms);
+    };
+
+    return (
+        <div
+            style={{
+                width,
+                height,
+            }}
+        >
+            <div>
+                <h1>Set Point Value: {fetchedData}</h1>
+            </div>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Enter Set Point Value:
+                        <input type="text" value={value} onChange={(e) => setValue(e.target.value)}/>
+                    </label>
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
+        </div>
+    );
 };
--- src/module.ts
+++ src/module.ts
@@ -1,61 +1,52 @@
-import { PanelPlugin } from '@grafana/data';
-import { SimpleOptions } from './types';
-import { SimplePanel } from './SimplePanel';
+import {PanelPlugin} from '@grafana/data';
+import {SimpleOptions} from './types';
+import {SimplePanel} from './SimplePanel';
 
-export const plugin = new PanelPlugin<SimpleOptions>(SimplePanel).setPanelOptions(builder => {
-  return builder
-    .addTextInput({
-      path: 'text',
-      name: 'Simple text option',
-      description: 'Description of panel option',
-      defaultValue: 'Default value of text input option',
-    })
-    .addBooleanSwitch({
-      path: 'showSeriesCount',
-      name: 'Show series counter',
-      defaultValue: false,
-    })
-    .addRadio({
-      path: 'seriesCountSize',
-      defaultValue: 'sm',
-      name: 'Series counter size',
-      settings: {
-        options: [
-          {
-            value: 'sm',
-            label: 'Small',
-          },
-          {
-            value: 'md',
-            label: 'Medium',
-          },
-          {
-            value: 'lg',
-            label: 'Large',
-          },
-        ],
-      },
-      showIf: config => config.showSeriesCount,
-    })
-    .addRadio({
-      path: 'color',
-      name: 'Circle color',
-      defaultValue: 'red',
-      settings: {
-        options: [
-          {
-            value: 'red',
-            label: 'Red',
-          },
-          {
-            value: 'green',
-            label: 'Green',
-          },
-          {
-            value: 'blue',
-            label: 'Blue',
-          },
-        ],
-      },
-    });
+export const plugin = new PanelPlugin<SimpleOptions>(SimplePanel).setPanelOptions((builder) => {
+    return builder
+        .addTextInput({
+            path: 'DataSourceName',
+            name: 'DataSource Plugin Name',
+            description: 'Enter the name of DataSource Plugin Used',
+            defaultValue: 'VOLTTRON',
+        })
+        .addTextInput({
+            path: 'VolttronNode',
+            name: 'VOLTTRON Node',
+            description: 'Enter the name of your VOLTTRON Node',
+            defaultValue: 'labtestlion',
+        })
+        .addTextInput({
+            path: 'URL',
+            name: 'API Endpoint Input',
+            description: 'Enter the API endpoint to send data',
+            defaultValue: 'Campus/Building1/Fake2/SampleWritableFloat1',
+        })
+        // .addBooleanSwitch({
+        //     path: 'showSeriesCount',
+        //     name: 'Show series counter',
+        //     defaultValue: false,
+        // })
+        // .addRadio({
+        //     path: 'seriesCountSize',
+        //     defaultValue: 'sm',
+        //     name: 'Series counter size',
+        //     settings: {
+        //         options: [
+        //             {
+        //                 value: 'sm',
+        //                 label: 'Small',
+        //             },
+        //             {
+        //                 value: 'md',
+        //                 label: 'Medium',
+        //             },
+        //             {
+        //                 value: 'lg',
+        //                 label: 'Large',
+        //             },
+        //         ],
+        //     },
+        //     showIf: (config) => config.showSeriesCount,
+        // })
 });
