0d083f499f5206b1eecb383c8b769832e06e6d77
Sadab <sadabmm@gmail.com>
2021-10-12 14:59:52 -0400

    last working code

--- src/SimplePanel.tsx
+++ src/SimplePanel.tsx
@@ -1,104 +1,120 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
-    PanelProps,
-    DataSourceInstanceSettings,
-    DataSourceJsonData,
-    DataQueryRequest, DataQueryResponse
+  PanelProps,
+  DataSourceInstanceSettings,
+  DataSourceJsonData,
+  DataQueryRequest,
+  DataQueryResponse,
 } from '@grafana/data';
-import {SimpleOptions, MyQuery} from 'types';
-import {getDataSourceSrv} from '@grafana/runtime';
-import {Observable} from 'rxjs';
+import { SimpleOptions, MyQuery } from 'types';
+import { getDataSourceSrv } from '@grafana/runtime';
+import { Observable } from 'rxjs';
 // import { css, cx } from 'emotion';
 // import { stylesFactory, useTheme } from '@grafana/ui';
+// import Select from 'react-select';
+// import Dropdown, { Option } from 'react-dropdown';
 
-interface Props extends PanelProps<SimpleOptions> {
-}
+interface Props extends PanelProps<SimpleOptions> {}
 
-export const SimplePanel: React.FC<Props> = ({options, data, width, height}) => {
-    const [value, setValue] = useState('');
-    const [fetchedData, setFetchedData] = useState('');
+export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
+  const [value, setValue] = useState('');
+  const [fetchedData, setFetchedData] = useState('');
+  // const [dropDownVal, setDropDownVal] = useState('');
 
-    // console.log('Data from Panel Props:')
-    // console.log(data)
-    let dssrv = getDataSourceSrv();
-    console.log('dssrv is: ');
-    console.log(dssrv);
-    console.log('getList returns: ');
-    console.log(dssrv.getList());
-    console.log('get("VOLTTRON") returns:');
-    console.log(dssrv.get('VOLTTRON'));
-    console.log('getInstanceSettings returns:');
-    const instance_settings: DataSourceInstanceSettings<DataSourceJsonData> | undefined =
-        dssrv.getInstanceSettings(options.DataSourceName);
-    console.log(instance_settings);
-    //const  url = '' ? instance_settings === undefined : instance_settings.url
+  // setDropDownVal('Whatever!');
+  // const opt = ['one', 'two', 'three'];
+  //
+  // const handleOptions: (e: string) => {
+  //   setDropDownVal(e: any): any;
+  // }
 
-    const handleSubmit = (evt: { preventDefault: () => void }) => {
-        evt.preventDefault();
-        alert(`Submitting Value: ${value}`);
+  // console.log('Data from Panel Props:')
+  // console.log(data)
+  let dssrv = getDataSourceSrv();
+  console.log('dssrv is: ');
+  console.log(dssrv);
+  console.log('getList returns: ');
+  console.log(dssrv.getList());
+  console.log('get("VOLTTRON") returns:');
+  console.log(dssrv.get('VOLTTRON'));
+  console.log('getInstanceSettings returns:');
+  const instance_settings: DataSourceInstanceSettings<DataSourceJsonData> | undefined = dssrv.getInstanceSettings(
+    options.DataSourceName
+  );
+  console.log(instance_settings);
+  //const  url = '' ? instance_settings === undefined : instance_settings.url
 
-        let dsprms = dssrv.get(options.DataSourceName).then(async (DataSource) => {
-            let q = {
-                refId: DataSource.id.toString(),
-                http_method: 'PUT',
-                route: options.platformDevice + options.URL,
-                data: {value: value},
-            } as MyQuery;
-            let request = {} as DataQueryRequest;
-            request.targets = [q];
-            let resp = await DataSource.query(request) as Observable<DataQueryResponse>;
-            console.log('resp from query is:');
-            console.log(resp);
+  const handleSubmit = (evt: { preventDefault: () => void }) => {
+    evt.preventDefault();
+    alert(`Submitting Value: ${value}`);
 
-            resp.subscribe({
-                next(obsData) {
-                    console.log("Observable Return is: ")
-                    console.log(obsData)
-                },
-                error(err) {
-                    console.error("Something went wrong: " + err);
-                    setFetchedData("Error Setting Value");
-                },
-                complete() {
-                    setFetchedData(value);
-                }
-            });
+    let dsprms = dssrv.get(options.DataSourceName).then(async (DataSource) => {
+      let q = {
+        refId: DataSource.id.toString(),
+        http_method: 'PUT',
+        route: options.platformDevice + options.URL,
+        data: { value: value },
+      } as MyQuery;
+      let request = {} as DataQueryRequest;
+      request.targets = [q];
+      let resp = (await DataSource.query(request)) as Observable<DataQueryResponse>;
+      console.log('resp from query is:');
+      console.log(resp);
 
-            // let setError = JSON.parse(resp.data[0].fields[0].values.buffer[0]);
-            // console.log('Value of set_error:');
-            // console.log(setError);
-            // console.log(setError[options.URL].set_error)
-            // if (setError[options.URL].set_error == null){
-            //     setFetchedData(value);
-            // } else {
-            //     setFetchedData("Error Setting Value")
-            // }
-            //onRunQuery()
-            //return request;
-        })
-        console.log("Datasource param response")
-        console.log(dsprms);
-    };
+      resp.subscribe({
+        next(obsData) {
+          console.log('Observable Return is: ');
+          let setError = JSON.parse(obsData.data[0].fields[0].values.buffer[0]);
+          let respKey = Object.keys(setError)[0];
+          console.log(setError);
+          if (setError[respKey].set_error == null) {
+            setFetchedData(value);
+          } else {
+            setFetchedData('Error Setting Value');
+          }
+        },
+        error(err) {
+          console.error('Something went wrong: ' + err);
+          setFetchedData('Error Setting Value');
+        },
+      });
 
-    return (
-        <div
-            style={{
-                width,
-                height,
-            }}
-        >
-            <div>
-                <h1>Set Point Value: {fetchedData}</h1>
-            </div>
-            <div>
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        Enter Set Point Value:
-                        <input type="text" value={value} onChange={(e) => setValue(e.target.value)}/>
-                    </label>
-                    <input type="submit" value="Submit"/>
-                </form>
-            </div>
-        </div>
-    );
+      // let setError = JSON.parse(resp.data[0].fields[0].values.buffer[0]);
+      // console.log('Value of set_error:');
+      // console.log(setError);
+      // console.log(setError[options.URL].set_error)
+      // if (setError[options.URL].set_error == null){
+      //     setFetchedData(value);
+      // } else {
+      //     setFetchedData("Error Setting Value")
+      // }
+      //onRunQuery()
+      //return request;
+    });
+    console.log('Datasource param response');
+    console.log(dsprms);
+  };
+
+  return (
+    <div
+      style={{
+        width,
+        height,
+      }}
+    >
+      {/*<Dropdown options={opt} onChange={handleOptions} value={dropDownVal} placeholder="Select an option" />;*/}
+      <div>
+        <h1>Set Point Value: {fetchedData}</h1>
+      </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Enter Set Point Value:
+            <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    </div>
+  );
 };
--- src/module.ts
+++ src/module.ts
@@ -1,52 +1,52 @@
-import {PanelPlugin} from '@grafana/data';
-import {SimpleOptions} from './types';
-import {SimplePanel} from './SimplePanel';
+import { PanelPlugin } from '@grafana/data';
+import { SimpleOptions } from './types';
+import { SimplePanel } from './SimplePanel';
 
 export const plugin = new PanelPlugin<SimpleOptions>(SimplePanel).setPanelOptions((builder) => {
-    return builder
-        .addTextInput({
-            path: 'DataSourceName',
-            name: 'DataSource Plugin Name',
-            description: 'Enter the name of DataSource Plugin Used',
-            defaultValue: 'VOLTTRON',
-        })
-        .addTextInput({
-            path: 'platformDevice',
-            name: 'Platform Device',
-            description: 'Enter the Platform Device Path',
-            defaultValue: '/platforms/labtestlion',
-        })
-        .addTextInput({
-            path: 'URL',
-            name: 'API Endpoint Input',
-            description: 'Enter the API endpoint to send data',
-            defaultValue: '/devices/Campus/Building1/Fake2/SampleWritableFloat1',
-        })
-        // .addBooleanSwitch({
-        //     path: 'showSeriesCount',
-        //     name: 'Show series counter',
-        //     defaultValue: false,
-        // })
-        // .addRadio({
-        //     path: 'seriesCountSize',
-        //     defaultValue: 'sm',
-        //     name: 'Series counter size',
-        //     settings: {
-        //         options: [
-        //             {
-        //                 value: 'sm',
-        //                 label: 'Small',
-        //             },
-        //             {
-        //                 value: 'md',
-        //                 label: 'Medium',
-        //             },
-        //             {
-        //                 value: 'lg',
-        //                 label: 'Large',
-        //             },
-        //         ],
-        //     },
-        //     showIf: (config) => config.showSeriesCount,
-        // })
+  return builder
+    .addTextInput({
+      path: 'DataSourceName',
+      name: 'DataSource Plugin Name',
+      description: 'Enter the name of DataSource Plugin Used',
+      defaultValue: 'VOLTTRON',
+    })
+    .addTextInput({
+      path: 'platformDevice',
+      name: 'Platform Device',
+      description: 'Enter the Platform Device Path',
+      defaultValue: '/platforms/labtestlion',
+    })
+    .addTextInput({
+      path: 'URL',
+      name: 'API Endpoint Input',
+      description: 'Enter the API endpoint to send data',
+      defaultValue: '/devices/Campus/Building1/Fake2/SampleWritableFloat1',
+    });
+  // .addBooleanSwitch({
+  //     path: 'showSeriesCount',
+  //     name: 'Show series counter',
+  //     defaultValue: false,
+  // })
+  // .addRadio({
+  //     path: 'seriesCountSize',
+  //     defaultValue: 'sm',
+  //     name: 'Series counter size',
+  //     settings: {
+  //         options: [
+  //             {
+  //                 value: 'sm',
+  //                 label: 'Small',
+  //             },
+  //             {
+  //                 value: 'md',
+  //                 label: 'Medium',
+  //             },
+  //             {
+  //                 value: 'lg',
+  //                 label: 'Large',
+  //             },
+  //         ],
+  //     },
+  //     showIf: (config) => config.showSeriesCount,
+  // })
 });
